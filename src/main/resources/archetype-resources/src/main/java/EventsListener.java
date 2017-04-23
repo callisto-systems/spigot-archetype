666ebf701cdc4e34f08eb2a910d54d26ff6c21dd
vasilache <mihai_vasilache@yahoo.com>
2017-04-24 00:32:46 +0300

    fix import

--- src/main/resources/archetype-resources/src/main/java/EventsListener.java
+++ src/main/resources/archetype-resources/src/main/java/EventsListener.java
@@ -3,6 +3,7 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
